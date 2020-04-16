9c6e828801febbf0590c2a567aa6524895b9543c
Christine <christine-xia@hotmail.com>
2020-04-16 00:26:50 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// CLICK TO HIDE ORB LANDING PAGE
+
+$('.orb').click(function() {
+	$('.title.main').animate({'opacity':'0'}, 1000);
+	$('.orb').animate({
+		'opacity': 0,
+		'cursor': 'default',
+		'z-index': '-200'
+	}, 2000);
+	// $(this).delay(2000).hide();
+	$('.footer').animate({'opacity':'1'}, 1500);
+
+	$('.cabinet').animate({'opacity':'1'}, 2500);
+	$('.cabinet').css({'z-index':'100', 'cursor':'grab'})
+	$('.cabinet').mousedown({'cursor':'grabbing'});
+})
+
+
+// DRAGGABLE CABINET
+
+$('.cabinet').draggable({
+	containment: "parent", 
+	scroll: false,
+    zIndex: 10000
+});
+// $('.cabinet').draggable();
