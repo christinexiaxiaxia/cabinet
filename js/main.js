8f3c364344d12667a983e593f7fca3c8b1fffc47
Christine <christine-xia@hotmail.com>
2020-04-16 08:22:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ $('.orb').click(function() {
 	$('.title.main').animate({'opacity':'0'}, 1000);
 	$('.orb').animate({
 		'opacity': 0,
-		'cursor': 'default',
 		'z-index': '-200'
 	}, 2000);
 	// $(this).delay(2000).hide();
 	$('.footer').animate({'opacity':'1'}, 1500);
 
 	$('.cabinet').animate({'opacity':'1'}, 2500);
-	$('.cabinet').css({'z-index':'100', 'cursor':'grab'})
+	// $('.cabinet').css({'z-index':'100', 'cursor':'grab'})
+	$('.cabinet').css({'z-index':'100'})
 	$('.cabinet').mousedown({'cursor':'grabbing'});
 })
 
@@ -19,17 +19,29 @@ $('.orb').click(function() {
 // QUESTION MARK HOVER/ABOUT
 
 $('.about').hover(function(){
-	$('.desc.main').css({'opacity':'1'})
+	$('.desc.main').css({'opacity':'1', 'z-index':'200'})
 }, function(){
-	$('.desc.main').css({'opacity':'0'})
+	$('.desc.main').css({'opacity':'0', 'z-index':'-200'})
 })
 
 
 // DRAGGABLE CABINET
 
 $('.cabinet').draggable({
-	containment: "parent", 
-	// scroll: false,
- //    zIndex: 10000
+	containment: "parent"
 });
-// $('.cabinet').draggable();
+
+
+// CLICK IMAGES TO OPEN NEW WINDOWS
+
+$('.saving-grace1').click(function(){
+	window.open('img/saving-grace1.jpg');
+})
+$('.saving-grace2').click(function(){
+	window.open('img/saving-grace2.jpg');
+})
+
+
+$('.classify-pain').click(function(){
+	window.open('img/classify-pain.png');
+})
